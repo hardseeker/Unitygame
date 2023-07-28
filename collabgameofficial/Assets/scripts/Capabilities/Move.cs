87349675e576108102e9a76ebe04ed7849c05ac0
hardseeker <gymbobjoebob@gmail.com>
2023-07-27 18:35:18 -0700

    directional

--- collabgameofficial/Assets/scripts/Capabilities/Move.cs
+++ collabgameofficial/Assets/scripts/Capabilities/Move.cs
@@ -9,6 +9,7 @@ public class NewBehaviourScript : MonoBehaviour
     [SerializeField, Range(0f, 100f)] private float maxAcceleration = 35f;
     [SerializeField, Range(0f, 100f)] private float maxAirAcceleration = 20f;
 
+    private float horizontal;
     private Vector2 direction;
     private Vector2 desiredVelocity;
     private Vector2 velocity;
@@ -18,16 +19,23 @@ public class NewBehaviourScript : MonoBehaviour
     private float maxSpeedChange;
     private float acceleration;
     private bool onGround;
+    public float speed;
+    private bool facingright = true;
 
     void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         ground = GetComponent<Ground>();
+        speed = velocity.magnitude;
     }
     void Update()
     {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        speed = velocity.magnitude;
+        animator.SetFloat("horzi movement", Mathf.Abs(speed));
         direction.x = input.RetrieveMoveInput();
         desiredVelocity = new Vector2(direction.x, 0f) * Mathf.Max(maxSpeed - ground.GetFriction(), 0f);
+        Flip();
 
     }
 
@@ -43,5 +51,15 @@ public class NewBehaviourScript : MonoBehaviour
         body.velocity = velocity;
     }
 
+    private void Flip()
+    {
+        if(facingright && horizontal <0f || !facingright &&horizontal >0f)
+        {
+            facingright = !facingright;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
 
 }
